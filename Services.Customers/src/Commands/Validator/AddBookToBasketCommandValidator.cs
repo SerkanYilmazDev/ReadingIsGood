9c1938d28794ffc26995ef7abcdfc8ad3734c424
serkan.yilmaz1 <serkan.yilmaz1@enerjisa.com>
2021-06-07 17:56:10 +0300

    Added Basket and Register User Validator

---
+++ Services.Customers/src/Commands/Validator/AddBookToBasketCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Services.Customers.Commands.Validator
+{
+    public class AddBookToBasketCommandValidator : AbstractValidator<AddBookToBasketCommand>
+    {
+        public AddBookToBasketCommandValidator()
+        {
+            RuleFor(c => c.Quantity).NotEmpty().NotNull().GreaterThan(0);
+        }
+    }
+}
---
+++ Services.Identity/src/Commands/Validator/RegisterUserCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Services.Identity.Commands.Validator
+{
+    public class RegisterUserCommandValidator : AbstractValidator<RegisterUserCommand>
+    {
+        public RegisterUserCommandValidator()
+        {
+            RuleFor(c => c.Email).NotEmpty().NotNull().EmailAddress();
+            RuleFor(c => c.FirstName).NotEmpty().NotNull();
+            RuleFor(c => c.LastName).NotEmpty().NotNull();
+            RuleFor(c => c.Password).NotEmpty().NotNull();
+            RuleFor(c => c.Address).NotEmpty().NotNull();
+        }
+    }
+}
